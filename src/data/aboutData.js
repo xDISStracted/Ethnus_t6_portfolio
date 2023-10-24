a75a7989f489a81e5b317b4f042daf131a7d417b
Prakhar Vishwas <144513343+xDISStracted@users.noreply.github.com>
2023-10-24 14:53:07 +0530

    Create aboutData.js

---
+++ src/data/aboutData.js
@@ -0,0 +1,6 @@
+export const aboutData = {
+    title: "Who I am",
+    description1: "My name is Prakhar. I'm a web designer and developer.",
+    description2: "I am a 3rd year Computer Science and Engineering student at VIT Vellore. I’m a detail-oriented and ambitious engineering student with a passion for obtaining accurate results and working on a team. Seeking an entry-level position in the engineering industry to pursue a career as a Software Engineer and improve my skill set.",
+    image: 1
+}
